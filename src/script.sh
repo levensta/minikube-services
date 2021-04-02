49da809bf6d341a3f2a76633a106150985f94a99
Lorent Evenstar <levensta@et-e1.kzn.21-school.ru>
2021-04-02 23:43:29 +0300

    metallb don't work

--- src/script.sh
+++ src/script.sh
@@ -2,10 +2,7 @@
 mkdir -p /run/nginx
 
 rm /etc/nginx/conf.d/*
-rm /etc/ssl/certs/ca-Certificates.crt
 mv /server/my_nginx.conf /etc/nginx/conf.d/
 openssl req -x509 -sha256 -nodes -days 421 -newkey rsa:4096 -keyout /etc/ssl/private/private.key -out /etc/ssl/certs/public.crt -subj "/C=RU/ST=Tatarstan/L=Kazan/O=Example-Certificates/CN=localhost.local"
 
 nginx -g 'daemon off;'
-
-bash
