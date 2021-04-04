fabc7513b8d1ddbc0f7af6367286eea41f7b99cb
levensta <levensta@student.21-school.ru>
2021-04-04 22:50:54 +0300

    add mysql pod

--- setup.sh
+++ setup.sh
@@ -4,6 +4,9 @@ docker pull metallb/speaker:v0.8.2
 docker pull metallb/controller:v0.8.2
 minikube addons enable metallb
 kubectl apply -f ./srcs/configmap.yaml
+kubectl apply -f ./srcs/pvolume.yaml
 
 docker build -t nginx_image ./srcs/nginx
 kubectl apply -f ./srcs/nginx/nginx.yaml
+docker build -t mysql_image ./srcs/mysql
+kubectl apply -f ./srcs/mysql/mysql.yaml
--- srcs/mysql/src/script.sh
+++ srcs/mysql/src/script.sh
@@ -1,7 +1,14 @@
-service mysql start
-mysql -e "CREATE DATABASE wordpress;"
-mysql -e "GRANT ALL ON wordpress.* TO 'root'@'localhost';"
-mysql -e "update mysql.user set plugin='mysql_native_password' where user='root';"
-mysql -e "FLUSH PRIVILEGES;"
+mv /server/mysql.cnf /etc/mysql/
+chmod 777 /etc/mysql/mysql.cnf
 
-bash
+openrc default
+/etc/init.d/mariadb setup
+rc-service mariadb start
+mariadb -e "CREATE DATABASE wordpress;" -u root --skip-password
+mariadb -e "CREATE USER 'root'@'%';" -u root --skip-password
+mariadb -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'root'@'%' WITH GRANT OPTION;" -u root --skip-password
+# mariadb -e "update mysql.user set plugin='mysql_native_password' where user='root';" -u root --skip-password
+mariadb -e "FLUSH PRIVILEGES;" -u root
+
+mysqld_safe --data='/var/lib/mysql'
+# bash
