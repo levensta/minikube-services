6947d58071008b868a9f3f6b2c80fe1564ee9dc8
levensta <levensta@student.21-school.ru>
2021-04-08 22:24:10 +0300

    add wordpress

--- srcs/mysql/src/script.sh
+++ srcs/mysql/src/script.sh
@@ -1,13 +1,16 @@
 mv /server/my.cnf /etc/mysql/
-chmod 777 /etc/mysql/my.cnf
+chmod 744 /etc/mysql/my.cnf
 
 openrc default
 /etc/init.d/mariadb setup
 rc-service mariadb start
-mariadb -e "CREATE DATABASE wordpress;" -u root --skip-password
-mariadb -e "CREATE USER 'root'@'%';" -u root --skip-password
-mariadb -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'root'@'%' WITH GRANT OPTION;" -u root --skip-password
-# mariadb -e "update mysql.user set plugin='mysql_native_password' where user='root';" -u root --skip-password
-mariadb -e "FLUSH PRIVILEGES;" -u root
+mysql -e "CREATE USER 'root'@'%';" -u root --skip-password
+mysql -e "CREATE DATABASE wordpress;" -u root --skip-password
+mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';" -u root --skip-password
+# mysql -e "update mysql.user set plugin='mysql_native_password' where user='root';" -u root --skip-password
+mysql -e "FLUSH PRIVILEGES;" -u root
 
-mysqld_safe --data='/var/lib/mysql'
+mysql wordpress -u root < /server/wordpress.sql
+rc-service mariadb stop
+/usr/bin/mysqld_safe --datadir='/var/lib/mysql'
+# bash
--- srcs/wp/src/script.sh
+++ srcs/wp/src/script.sh
@@ -2,7 +2,7 @@ mkdir -p /run/nginx
 mkdir -p /run/php
 mkdir -p /var/log/supervisor
 
-chmod -R 777 /var/www/*
+chmod -R 744 /var/www/*
 mv /server/wp_nginx.conf /etc/nginx/conf.d/
 wget https://wordpress.org/latest.tar.gz
 tar -xzf latest.tar.gz -C /var/www/
