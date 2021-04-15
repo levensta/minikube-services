d81be8f66ed3639df36e74337964005d10b238a7
levensta <levensta@student.21-school.ru>
2021-04-15 22:20:07 +0300

    fix influxdb

--- srcs/influxdb/src/script.sh
+++ srcs/influxdb/src/script.sh
@@ -1,17 +1,17 @@
 cd server
-
+apk add influxdb
 wget -q -O /etc/apk/keys/sgerrand.rsa.pub https://alpine-pkgs.sgerrand.com/sgerrand.rsa.pub
 wget https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.33-r0/glibc-2.33-r0.apk
 apk add glibc-2.33-r0.apk
 
-wget https://dl.influxdata.com/influxdb/releases/influxdb2-2.0.4-linux-amd64.tar.gz
+# wget https://dl.influxdata.com/influxdb/releases/influxdb2-2.0.4-linux-amd64.tar.gz
 wget https://dl.influxdata.com/telegraf/releases/telegraf-1.18.1_linux_amd64.tar.gz
 tar xf telegraf-1.18.1_linux_amd64.tar.gz
-tar xzf influxdb2-2.0.4-linux-amd64.tar.gz
-rm -rf influxdb2-2.0.4-linux-amd64.tar.gz
+# tar xzf influxdb2-2.0.4-linux-amd64.tar.gz
+# rm -rf influxdb2-2.0.4-linux-amd64.tar.gz
 rm -rf telegraf-1.18.1_linux_amd64.tar.gz
 
-mv ./influxdb2-2.0.4-linux-amd64 /etc/influxdb/
+# mv ./influxdb2-2.0.4-linux-amd64 /etc/influxdb/
 cp -r ./telegraf-1.18.1/* /
 
 mkdir -p /var/log/supervisor
@@ -21,3 +21,4 @@ cd /
 /usr/bin/supervisord -c /etc/supervisord.conf
 
 # bash
+# influxd run -config /etc/influxdb.conf
