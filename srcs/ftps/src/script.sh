f208f5ad52cadade74f55bef32283ed23c33122c
levensta <levensta@student.21-school.ru>
2021-04-19 21:53:25 +0300

    fix ftps

--- srcs/ftps/src/script.sh
+++ srcs/ftps/src/script.sh
@@ -3,7 +3,11 @@ touch /var/log/ftp/xferlog
 
 openssl req -x509 -sha256 -nodes -days 365 -newkey rsa:2048 -subj "/C=RU/ST=Tatarstan/L=Kazan/O=Example-Certificates/CN=localhost.local" \
 -keyout /etc/vsftpd/vsftpd.pem -out /etc/vsftpd/vsftpd.pem
+# adduser -D levensta
+# echo levensta > /etc/vsftpd/vsftpd.userlist
+echo "root:root" | chpasswd
+mv /server/vsftpd.conf /etc/vsftpd/vsftpd.conf
 
-mv /server/vsftpd.conf /etc/vsftpd/
+
 echo "start vsftpd"
 vsftpd /etc/vsftpd/vsftpd.conf
